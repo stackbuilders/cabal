ad876955dfc713b754be5174c312898e31b1f33c
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-10-16 12:10:46 -0500

    Add helper methods to extract credentials

--- cabal-install/src/Distribution/Client/Types/Credentials.hs
+++ cabal-install/src/Distribution/Client/Types/Credentials.hs
@@ -2,13 +2,28 @@ module Distribution.Client.Types.Credentials (
     Auth (..),
     Username (..),
     Password (..),
-    Token (..)
+    Token (..),
+    unAuthUsername,
+    unAuthPassword,
+    unAuthToken
 ) where
 
-import Prelude (String)
+import Prelude (String, Maybe (..))
 
 data Auth = AuthCredentials Username Password | AuthToken Token
 
 newtype Username = Username { unUsername :: String }
 newtype Password = Password { unPassword :: String }
 newtype Token = Token { unToken :: String }
+
+unAuthUsername :: Auth -> Maybe Username
+unAuthUsername (AuthCredentials username _) = Just username
+unAuthUsername _ = Nothing
+
+unAuthPassword :: Auth -> Maybe Password
+unAuthPassword (AuthCredentials _ password) = Just password
+unAuthPassword _ = Nothing
+
+unAuthToken :: Auth -> Maybe Token
+unAuthToken (AuthToken token) = Just token
+unAuthToken _ = Nothing 
