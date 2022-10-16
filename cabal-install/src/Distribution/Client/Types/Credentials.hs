2a32d40d5a272d23ae4f962d133e7b16f069b823
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-10-16 11:57:13 -0500

    Add Auth sumtype

--- cabal-install/src/Distribution/Client/Types/Credentials.hs
+++ cabal-install/src/Distribution/Client/Types/Credentials.hs
@@ -1,10 +1,13 @@
 module Distribution.Client.Types.Credentials (
+    Auth (..),
     Username (..),
     Password (..),
     Token (..)
 ) where
 
 import Prelude (String)
+
+data Auth = AuthCredentials Username Password | AuthToken Token
 
 newtype Username = Username { unUsername :: String }
 newtype Password = Password { unPassword :: String }
