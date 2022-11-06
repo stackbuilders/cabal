313714523f5497e47645930c009969de60fe68e2
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-11-06 12:22:55 -0500

    Testing both Auth constructors

--- cabal-install/tests/UnitTests/Distribution/Client/HttpUtils.hs
+++ cabal-install/tests/UnitTests/Distribution/Client/HttpUtils.hs
@@ -4,6 +4,7 @@ module UnitTests.Distribution.Client.HttpUtils
 
 import Data.Maybe (fromJust)
 import Distribution.Client.HttpUtils (HttpTransport(..), configureTransport)
+import Distribution.Client.Types.Credentials
 import Distribution.Verbosity (silent, verbose)
 import Network.URI (parseURI)
 import Test.Tasty
@@ -13,13 +14,24 @@ tests :: TestTree
 tests = testGroup "HttpUtils"
   [ testGroup "postHttpFile"
       [ testCase "curl" testPostHttpFileCurl
+      , testCase "curl" testPostHttpFileCurl2
       ]
   ]
 
 testPostHttpFileCurl :: IO ()
 testPostHttpFileCurl = do
   let uri = fromJust $ parseURI "https://hackage.haskell.org/packages/candidates"
+      auth = AuthToken $ Token "foo"
   transport <- configureTransport silent [] (Just "curl")
-  (code, body) <- postHttpFile transport verbose uri "tests/fixtures/files/fake.tar.gz" Nothing
-  print body
-  code @=? 200
+  (code, body) <- postHttpFile transport verbose uri "tests/fixtures/files/fake.tar.gz" (Just auth)
+  code @=? 401
+  body @=? "Error: Bad auth token\n"
+
+testPostHttpFileCurl2 :: IO ()
+testPostHttpFileCurl2 = do
+  let uri = fromJust $ parseURI "https://hackage.haskell.org/packages/candidates"
+      auth = AuthCredentials $ Credentials (Username "foo") (Password "bar")
+  transport <- configureTransport silent [] (Just "curl")
+  (code, body) <- postHttpFile transport verbose uri "tests/fixtures/files/fake.tar.gz" (Just auth)
+  code @=? 401
+  body @=? "Error: Username or password incorrect\n"
