26e27d101ef3fbb8e2a60965ef6992fdc7b95df8
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-11-06 23:56:40 -0500

    Refactor tests

--- cabal-install/tests/UnitTests/Distribution/Client/HttpUtils.hs
+++ cabal-install/tests/UnitTests/Distribution/Client/HttpUtils.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module UnitTests.Distribution.Client.HttpUtils
   ( tests
   ) where
 
+import Control.Exception
 import Data.List (isInfixOf)
 import Data.Maybe (fromJust)
 import Distribution.Client.HttpUtils (HttpTransport(..), configureTransport)
@@ -14,39 +17,41 @@ import Test.Tasty.HUnit
 tests :: TestTree
 tests = testGroup "HttpUtils"
   [ testGroup "postHttpFile"
-      [ testGroup "curl"
-          [ testCase "credentials" $ testPostHttpFileCurl
-              (AuthToken $ Token "foo")
-              "Bad auth token"
-          , testCase "token" $ testPostHttpFileCurl
-              (AuthCredentials $ Credentials (Username "foo") (Password "bar"))
-              "Username or password incorrect"
-          ]
-      -- , testPostHttpFile "wget"
-      -- , testPostHttpFile "powershell"
-      , testGroup "plain-http"
-          [ testCase "credentials" $ testPostHttpFilePlainHttp
-              (AuthToken $ Token "foo")
-              (401, "Bad auth token")
-          , testCase "token" $ testPostHttpFilePlainHttp
-              (AuthCredentials $ Credentials (Username "foo") (Password "bar"))
-              (400, "Bad Request")
-          ]
+      [ testPostHttpFileByProgram "curl"
+          (401, "Username or password incorrect")
+          (401, "Bad auth token")
+      , testPostHttpFileByProgram "wget"
+          (401, "Username/Password Authentication Failed")
+          (401, "Username/Password Authentication Failed")
+      , testPostHttpFileByProgram "powershell"
+          (401, "")
+          (401, "")
+      , testPostHttpFileByProgram "plain-http"
+          (400, "Bad Request")
+          (401, "Bad auth token")
       ]
   ]
 
-testPostHttpFileCurl :: Auth -> String -> IO ()
-testPostHttpFileCurl auth message = do
-  let uri = fromJust $ parseURI "https://hackage.haskell.org/packages/candidates"
-  transport <- configureTransport silent [] (Just "curl")
-  (code, body) <- postHttpFile transport silent uri "tests/fixtures/files/fake.tar.gz" (Just auth)
-  code @?= 401
-  isInfixOf message body @? "Expect \"" ++ body ++ "\" to contain \""  ++ message ++ "\""
+testPostHttpFileByProgram :: String -> (Int, String) -> (Int, String) -> TestTree
+testPostHttpFileByProgram program credentialsExpectations tokenExpectations =
+  testGroup program
+    [ testCase "credentials" $ testPostHttpFileByAuth program
+        (AuthCredentials $ Credentials (Username "foo") (Password "bar"))
+        credentialsExpectations
+    , testCase "token" $ testPostHttpFileByAuth program
+        (AuthToken $ Token "foo")
+        tokenExpectations
+    ]
 
-testPostHttpFilePlainHttp :: Auth -> (Int, String) -> IO ()
-testPostHttpFilePlainHttp auth (expectedCode, message) = do
+testPostHttpFileByAuth :: String -> Auth -> (Int, String) -> IO ()
+testPostHttpFileByAuth program auth (expectedCode, message) = do
   let uri = fromJust $ parseURI "http://hackage.haskell.org/packages/candidates"
-  transport <- configureTransport silent [] (Just "plain-http")
-  (code, body) <- postHttpFile transport silent uri "tests/fixtures/files/fake.tar.gz" (Just auth)
-  code @?= expectedCode
-  isInfixOf message body @? "Expect \"" ++ body ++ "\" to contain \""  ++ message ++ "\""
+  transport <- configureTransport silent [] (Just program)
+  eResponse <- try $ postHttpFile transport silent uri "tests/fixtures/files/fake.tar.gz" (Just auth)
+  case eResponse of
+    (Left (err :: SomeException)) -> do
+      let body = displayException err
+      isInfixOf message body @? "Expect \"" ++ body ++ "\" to contain \""  ++ message ++ "\""
+    (Right (code, body)) -> do
+      code @?= expectedCode
+      isInfixOf message body @? "Expect \"" ++ body ++ "\" to contain \""  ++ message ++ "\""
