57788cf4deff96b905d7a8cdf8167693ab681deb
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2022-11-08 00:43:16 -0500

    Refactor HttpUtils tests

--- cabal-install/tests/UnitTests/Distribution/Client/HttpUtils.hs
+++ cabal-install/tests/UnitTests/Distribution/Client/HttpUtils.hs
@@ -17,38 +17,38 @@ import Test.Tasty.HUnit
 tests :: TestTree
 tests = testGroup "HttpUtils"
   [ testGroup "postHttpFile"
-      [ testPostHttpFileByProgram "curl"
+      [ postHttpFileTests "curl"
           (401, "Username or password incorrect")
           (401, "Bad auth token")
-      , testPostHttpFileByProgram "wget"
+      , postHttpFileTests "wget"
           (401, "Username/Password Authentication Failed")
           (401, "Username/Password Authentication Failed")
-      , testPostHttpFileByProgram "powershell"
+      , postHttpFileTests "powershell"
           (401, "")
           (401, "")
-      , testPostHttpFileByProgram "plain-http"
+      , postHttpFileTests "plain-http"
           (400, "Bad Request")
           (401, "Bad auth token")
       ]
   ]
 
-testPostHttpFileByProgram :: String -> (Int, String) -> (Int, String) -> TestTree
-testPostHttpFileByProgram program credentialsExpectations tokenExpectations =
+postHttpFileTests :: String -> (Int, String) -> (Int, String) -> TestTree
+postHttpFileTests program credentialsExpectations tokenExpectations =
   testGroup program
-    [ testCase "credentials" $ testPostHttpFileByAuth program
+    [ testCase "credentials" $ testPostHttpFile program
         (AuthCredentials $ Credentials (Username "foo") (Password "bar"))
         credentialsExpectations
-    , testCase "token" $ testPostHttpFileByAuth program
+    , testCase "token" $ testPostHttpFile program
         (AuthToken $ Token "foo")
         tokenExpectations
     ]
 
-testPostHttpFileByAuth :: String -> Auth -> (Int, String) -> IO ()
-testPostHttpFileByAuth program auth (expectedCode, message) = do
+testPostHttpFile :: String -> Auth -> (Int, String) -> IO ()
+testPostHttpFile program auth (expectedCode, message) = do
   let uri = fromJust $ parseURI "http://hackage.haskell.org/packages/candidates"
   transport <- configureTransport silent [] (Just program)
-  eResponse <- try $ postHttpFile transport silent uri "tests/fixtures/files/fake.tar.gz" (Just auth)
-  case eResponse of
+  response <- try $ postHttpFile transport silent uri "tests/fixtures/files/fake.tar.gz" (Just auth)
+  case response of
     (Left (err :: SomeException)) -> do
       let body = displayException err
       isInfixOf message body @? "Expect \"" ++ body ++ "\" to contain \""  ++ message ++ "\""
